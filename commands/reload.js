fbabd39b1f91571e298cc9240dfa2a050cf7731e
simond-hue <achipowa205@gmail.com>
2019-10-16 23:27:01 +0200

    reload fix

--- commands/reload.js
+++ commands/reload.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
         .setColor("#FFFFFF")
         .addField("Újraindítás...", "Újraindítás..."))
     .then(msg => bot.destroy())
-    .then(() => bot.login(botconfig.token));
+    .then(() => bot.login(process.env.token));
     bot.commands = new Discord.Collection();
     fs.readdir("./commands", (err, files) => {
         if (err) console.log(err);
