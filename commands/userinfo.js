5eb1d338c2b3a5443a9c7cb58ba941a1c1c44985
Maris Klava <maris.klava@gmail.com>
2019-10-31 21:02:11 +0200

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -3,7 +3,6 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
       let user = message.mentions.users.first() || message.author;
       let member = await message.guild.fetchMember(user);
-
       let userembed = new Discord.RichEmbed()
         .setDescription("User Információ")
         .setColor("#58C2AB")
